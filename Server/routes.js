6a5cd1f6890e8966017da413bd979265df93550e
Ashley <Ashleystanbridge@icloul.com>
2016-02-27 19:12:24 +1300

    add handlebars blog format

--- Server/routes.js
+++ Server/routes.js
@@ -1,5 +1,12 @@
 var exphbs  = require('express-handlebars');
+var superagent = require('superagent')
+var fs = require('fs')
+var path = require('path')
 exports = module.exports = function (app) {
+
+var blogPath = path.join(__dirname, '../data/db.json')
+
+  app.get('/blog')
 
   app.get('/', function (req, res) {
     res.render('home');
@@ -10,14 +17,26 @@ exports = module.exports = function (app) {
     res.render('portfolio');
 });
 
-  app.get('/portfolio', function (req, res) {
-    res.render('/projects');
+  app.get('/projects', function (req, res) {
+    res.render('projects');
 });
 
 
   app.get('/blog', function (req, res) {
-    res.render('/blog');
+    fs.readFile(blogPath, 'utf8', function (err, data) {
+      if (err) throw err
+        var blogs = JSON.parse(data).blogs
+      console.log('blogs', blogs)
+      res.render('blog', {blog: blogs});
+    })
+    //res.render('blog', {name: data});
+});
+
+    app.get('/gallery', function (req, res) {
+    res.render('gallery');
 });
 //Add new routes here
 
 }
+
+
